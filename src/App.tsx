ac6377cfe364f2567132f525c88fca3c6a874734
yangabrp <yg@forpeople.io>
2022-12-27 16:00:41 -0300

    feat: atualizando template para prover as funções criadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
-import {useSelector} from 'react-redux'
-import './App.css';
-import { useMusic } from './redux/sliceMusics';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createNoSubstitutionTemplateLiteral } from "typescript";
+import "./App.css";
+import { addMusic, deleteMusic, favMusic, useMusic } from "./redux/sliceMusics";
 
 function App() {
-  const musics = useSelector(useMusic)
-
+  const [inputValue, setInputValue] = useState<string>();
+  const musics = useSelector(useMusic);
+  const dispatch = useDispatch();
+  const deleteItem = (arr: any, index: number) => {
+    return [...arr.slice(0, index), ...arr.slice(index + 1)];
+  };
   return (
     <div>
-      {musics.map((music) => {
-        return(
-          <li>{music.name}</li>
-        )
+      {musics.map((music, index) => {
+        return (
+          <div>
+            <span>{music.name}</span>
+            <button onClick={() => dispatch(favMusic(music.name))}>
+              {music.favorite ? "Desfavoritado" : "Favoritado"}
+            </button>
+            <button
+              onClick={() => {
+                dispatch(deleteMusic(index));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
       })}
+      <div>
+        <input
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button onClick={() => dispatch(addMusic(inputValue))}>
+          Adicionar música
+        </button>
+      </div>
     </div>
   );
 }
